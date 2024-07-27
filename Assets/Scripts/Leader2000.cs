023d274b991aa0c87977d961e72081e7e945a299
Jesus Gonzalez <jesugo11@ucm.es>
2024-07-27 12:14:48 +0200

    Leaderboard hecha cheeck

---
+++ Assets/Scripts/Leader2000.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Dan.Main;
+using System.Linq;
+using System;
+public class Leader2000 : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<TextMeshProUGUI> names;
+    [SerializeField]
+    private List<TextMeshProUGUI> scores;
+
+    private string publicLeaderboardKey = "6b16819a5210deef139c15238e3dde03661f79b49950dbc710504ad7520a9469";
+    public void getLeaderboard()
+    {
+        LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, (msg) =>
+        {
+            for (int i = 0; i < Math.Min(names.Count,msg.Length); ++i)
+            {
+                names[i].text = msg[i].Username;
+                scores[i].text = msg[i].Score.ToString();
+            }
+        });
+       
+    }
+
+    public void SetLeaderboardEntry(string username, int score)
+    {
+       LeaderboardCreator.UploadNewEntry(publicLeaderboardKey, username, score, ((msg) =>
+        {
+            getLeaderboard();
+        }));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
