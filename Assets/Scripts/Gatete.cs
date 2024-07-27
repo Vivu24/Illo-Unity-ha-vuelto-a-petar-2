2bee9c34570b2cb41a87ecd242ca212356b62079
Jimbo <javicentear@gmail.com>
2024-07-27 20:10:37 +0200

    gatooooooooooooooo

--- Assets/Scripts/Gatete.cs
+++ Assets/Scripts/Gatete.cs
@@ -6,6 +6,13 @@ using UnityEngine;
 public class Gatete : MonoBehaviour
 {
     [SerializeField] private float _force;
+
+    [SerializeField] private Sprite _normal;
+    [SerializeField] private Sprite _delante;
+    [SerializeField] private Sprite _detras;
+
+    private float _timer = 1;
+
     private int[] _lado = {1, -1};
 
     void OnTriggerEnter(Collider other)
@@ -13,8 +20,29 @@ public class Gatete : MonoBehaviour
         if (other.gameObject.layer == 6)
         {
             Debug.Log("ostia");
-            // TODO: Play animation
-            other.gameObject.transform.parent.GetComponent<Rigidbody>().AddForce(0, 0, _force * _lado[Random.Range(0, 2)]);
+            int lado = _lado[Random.Range(0, 2)];
+            other.gameObject.transform.parent.GetComponent<Rigidbody>().AddForce(0, 50, _force * lado);
+            if (lado == 1)
+            {
+                GetComponent<SpriteRenderer>().sprite = _delante;
+            }
+            else if (lado == -1)
+            {
+                GetComponent<SpriteRenderer>().sprite = _detras;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (GetComponent<SpriteRenderer>().sprite == _detras || GetComponent<SpriteRenderer>().sprite == _delante)
+        {
+            if (_timer <= 0)
+            {
+                GetComponent<SpriteRenderer>().sprite = _normal;
+                _timer = 1;
+            }
+            _timer -= Time.deltaTime;
         }
     }
 }
