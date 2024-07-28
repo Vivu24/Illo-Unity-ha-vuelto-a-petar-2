a3e610b9adf6350a88f741c55f8063d88389d624
Jimbo <javicentear@gmail.com>
2024-07-28 11:58:10 +0200

    jh

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,32 +9,42 @@ public class MenuManager : MonoBehaviour
     [SerializeField]
     public Animator panel;
 
+    private bool _hide = false;
+
+    private Vector3 _hided;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _hided = new Vector3(transform.position.x, transform.position.y + 300f, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_hide)
+        {
+            transform.position = Vector3.Lerp(transform.position, _hided, 10f * Time.deltaTime);
+        }
     }
+
     IEnumerator Fade()
     {
         panel.SetTrigger("fade");
         Debug.Log("holi");
         yield return new WaitForSeconds(3);
-        SceneManager.LoadScene("MainPrueba");
-
+        SceneManager.LoadSceneAsync("MainPrueba");
+        panel.SetTrigger("fade");
     }
     public void LoadPlayState()
     {
+        _hide = true;
         StartCoroutine(Fade());        
     }
 
     public void LoadLeadBoardState()
     {
+        _hide = true;
         SceneManager.LoadScene("LeaderBoardScene");
     }
 
